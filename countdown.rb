ff8c5ce765ff036567bc18af4407960949390cd8
GithinjiV <githinjigicheru@gmail.com>
2023-07-26 22:15:29 +0300

    Test Complete

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
 
 def countdown
+    x = 1
+while x < 10
+  puts "#{x} is less than 10"
+  x += 1
 end
+end
+
+def countdown_with_sleep   
+    x = 10
+while x > 0
+  puts "#{x} seconds remaining!!"
+  x -= 1
+  sleep(1)
+end
+puts "Happy New Year!!"
+end
+
+countdown_with_sleep
